b7db8fa1d32ec7d921339389c8b2ca255d91e592
Christa Davis <christadavis.301@gmail.com>
2022-08-02 01:26:30 -0400

    Added files and fixed features

--- notesAndTasks.js
+++ notesAndTasks.js
@@ -1,33 +1,31 @@
+/*
+GUI Programming II HW 5
+Christa Davis
+DASH-Meet notesAndTasks.js
+Javascript for notes and tasks sections of dashboard
+Sources: https://www.geeksforgeeks.org/how-to-create-notes-taking-site-using-html-bootstrap-and-javascript/
+*/
 showNotes();
-
-// If user adds a note, add it to the localStorage
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function(e) {
-    let addTxt = document.getElementById("addTxt");
+    let newText = document.getElementById("addTxt");
     let notes = localStorage.getItem("notes");
-
-    if (notes == null) notesObj = [];
+    if(notes == null) notesObj = [];
     else notesObj = JSON.parse(notes);
-
-    notesObj.push(addTxt.value);
+    notesObj.push(newText.value);
     localStorage.setItem("notes", JSON.stringify(notesObj));
-    addTxt.value = "";
-
+    newText.value = "";
     showNotes();
 });
 
-// Function to show elements from localStorage
 function showNotes() {
     let notes = localStorage.getItem("notes");
-
     if (notes == null) notesObj = [];
     else notesObj = JSON.parse(notes);
-
     let html = "";
-
     notesObj.forEach(function(element, index) {
-        html += `<div class="noteCard my-2 mx-2 card"
-            style="width: 18rem;">
+        html += `<div class="noteCard my-4 mx-4 card"
+            style="width: 20rem;">
                 <div class="card-body">
                     <h5 class="card-title">
                         Note ${index + 1}
@@ -35,7 +33,6 @@ function showNotes() {
                     <p class="card-text">
                         ${element}
                     </p>
-
                   <button id="${index}" onclick=
                     "deleteNote(this.id)"
                     class="btn btn-primary">
@@ -44,26 +41,16 @@ function showNotes() {
             </div>
         </div>`;
     });
-
     let notesElm = document.getElementById("notes");
-
     if (notesObj.length != 0) notesElm.innerHTML = html;
-    else
-        notesElm.innerHTML = `Nothing to show!
-        Use "Add a Note" section above to add notes.`;
+    else notesElm.innerHTML = ``;
 }
 
-// Function to delete a note
 function deleteNote(index) {
     let notes = localStorage.getItem("notes");
-
     if (notes == null) notesObj = [];
     else notesObj = JSON.parse(notes);
-
     notesObj.splice(index, 1);
-
-    localStorage.setItem("notes",
-        JSON.stringify(notesObj));
-
+    localStorage.setItem("notes", JSON.stringify(notesObj));
     showNotes();
 }
